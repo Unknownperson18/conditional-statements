52169d3668e4501bc404cc8073cdf9548baa9aa5
Unknownperson18 <86601146+Unknownperson18@users.noreply.github.com>
2021-06-28 16:25:19 +0530

    Delete number and days.py

--- number and days.py
+++
@@ -1,9 +0,0 @@
-n= int( input("enter the number:"))
-if(n==1 or n==3 or n==5 or n==7 or n==8 or n==10 or n==12):
-	print(n,"this month contains 31 days:")
-elif(n==2):
-	print(n,"this month contains 29 days:")
-elif(n==4 or n==6 or n==9 or n==11):
-	print(n,"this month contains 30 days:")
-else:
-	print("not a month:")
